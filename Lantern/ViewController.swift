e9dc8c8d7ff9ca5f2ea48d7ff2c78d2eaa8795df
dannertech <danner@danner.tech>
2020-03-08 03:48:17 -0400

    initial project files

---
+++ Lantern/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Lantern
+//
+//  Created by Diamonique Danner on 3/8/20.
+//  Copyright © 2020 Danner Opp., LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
